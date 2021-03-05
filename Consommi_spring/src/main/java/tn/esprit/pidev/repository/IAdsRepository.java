609412ab15a12bdb564510c6fcc16a58da4f4abe
AymenOuertani9 <aymen.ouertani@esprit.tn>
2021-03-05 19:46:18 +0100

    comm 51

--- Consommi_spring/src/main/java/tn/esprit/pidev/repository/IAdsRepository.java
+++ Consommi_spring/src/main/java/tn/esprit/pidev/repository/IAdsRepository.java
@@ -1,5 +1,8 @@
 package tn.esprit.pidev.repository;
 
+import java.util.Date;
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +10,8 @@ import tn.esprit.pidev.entities.Ads;
 
 @Repository
 public interface IAdsRepository extends CrudRepository<Ads,Integer> {
+	
+	List<Ads> getAdsByStartDate(Date SDate);
+	List<Ads> getAdsByFinishDate(Date FDate);
 
 }
--- Consommi_spring/src/main/java/tn/esprit/pidev/service/IAdsService.java
+++ Consommi_spring/src/main/java/tn/esprit/pidev/service/IAdsService.java
@@ -1,11 +1,18 @@
 package tn.esprit.pidev.service;
 
+import java.util.Date;
+import java.util.List;
+
 import tn.esprit.pidev.entities.Ads;
 
 public interface IAdsService {
 	
 	void AddAd(Ads ads);
 	void ModAd(int AdsId,Ads ads);
-	
+	void Mod_SDate(int AdsID,Date new_SDate);
+	void Mod_FDate(int AdsId,Date new_FDate);
+	List<Ads> GetAllAds();
+	List<Ads> GetAdsByFDate(Date FDate);
+	List<Ads> GetAdsBySDate(Date SDate);
 
 }
